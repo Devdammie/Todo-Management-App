0a3cce4d397d0555506aa62514b01a55143e68d6
Devdammie <136215055+Devdammie@users.noreply.github.com>
2025-06-30 08:13:30 -0700

    Update Create.jsx

--- todoList/src/Create.jsx
+++ todoList/src/Create.jsx
@@ -1,3 +1,4 @@
+/*
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
@@ -34,3 +35,80 @@ function Create (){
 }
 
 export default Create
+*/
+
+/*
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+
+function Create (){
+   // const handleAdd = () => {
+        // Logic to add a new todo item
+     //   console.log("Todo added");
+   // }
+    const [task, setTask] = useState(''); // State to hold the input value
+    const handleAdd = () => {
+        if (task.trim() === '') {
+            alert('Please enter a valid task');
+            return;
+        }
+        axios.post('http://localhost:3001/add', {task })
+            .then(response => {
+                location.reload()
+               
+            })
+            .catch(error => {
+                console.error('Error adding todo:', error);
+            });
+    };
+    return (
+        <div className='create_form'> 
+        < input  type="text" name="" id="" placeholder='Enter a text' onChange={(e) => setTask(e.target.value)} />
+        <button  type="button" onClick={handleAdd}>Add</button>
+        
+         </div>
+    )
+}
+
+export default Create
+
+*/
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Create({ url }) {
+  const [task, setTask] = useState('');
+
+  const handleAdd = () => {
+    if (task.trim() === '') {
+      alert('Please enter a valid task');
+      return;
+    }
+
+    axios
+      .post(`${url}/add`, { task })
+      .then(() => location.reload())
+      .catch(error => {
+        console.error('Error adding todo:', error);
+      });
+  };
+
+  return (
+    <div className="create_form">
+      <input
+        type="text"
+        placeholder="Enter a task"
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default Create;
