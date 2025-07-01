c5dd95f7506105720a11e6a5b9baa81ce483a1b2
Dammie <damilolajimoh2020@gmail.com>
2025-07-01 16:06:12 -0700

    update server

--- Server/Models/Todo.js
+++ Server/Models/Todo.js
@@ -5,7 +5,14 @@ const mongoose = require('mongoose');
 const TodoSchema = new mongoose.Schema({
   task: { type: String, required: true, trim: true, maxlength: 100, minlength: 1 },
   assignedTo: { type: String, required: true, trim: true, maxlength: 50, minlength: 1 },
-  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' },
+  
+  priority: {
+  type: String,
+  enum: ['High', 'Medium', 'Low', 'high', 'medium', 'low'],
+  default: 'Medium',
+},
+
+
   done: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
